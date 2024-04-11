5e83788f7ae177339336491192ee4a0cff5faeb6
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-10 17:43:29 -0700

    set up our variables for our app in tailwind config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        spanishGray: 'hsl(0, 0%, 60%)',
+        sonicSilver: 'hsl(0, 0%, 47%)',
+        eerieBlack: 'hsl(0, 0%, 13%)',
+        salmonPink: 'hsl(353, 100%, 78%)',
+        sandyBrown: 'hsl(29, 90%, 65%)',
+        bitterSweet: 'hsl(0, 100%, 70%)',
+        oceanGreen: 'hsl(152, 51%, 52%)',
+        cultured: 'hsl(0, 0%, 93%)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
