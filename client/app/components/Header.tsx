315845251a1be64058cfa0974165c402aef72dbf
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-10 18:11:17 -0700

    top of the header is done

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -4,14 +4,25 @@ import { socialLinks } from '../data/data'
 const Header = () => {
   return (
     <header>
-      <div>
-        <nav>
+      <div className='flex items-center justify-between border-b px-8 pb-2'>
+        <nav className='flex items-center gap-2'>
           {socialLinks.map((link, idx) => (
-            <Link href={link.href} key={idx}>
-              <link.icon />
+            <Link
+              href={link.href}
+              key={idx}
+              target='_blank'
+              className='bg-cultured p-1 rounded hover:scale-105 active:scale-90 transition-all'
+            >
+              <link.icon className='text-sonicSilver' />
             </Link>
           ))}
         </nav>
+        <div className='flex items-center gap-1 text-[12px]'>
+          <span className='font-medium text-sonicSilver'>FREE SHIPPING</span>
+          <span>-</span>
+          <p className='text-spanishGray'>THIS WEEK ORDER OVER $55</p>
+        </div>
+        <div></div>
       </div>
     </header>
   )
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,7 +2,7 @@ import Header from './components/Header'
 
 const Home = () => {
   return (
-    <main>
+    <main className='py-2'>
       <Header />
     </main>
   )
