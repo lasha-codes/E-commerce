cb944e701d6a095da1a5bd07b389c928b4749df0
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-10 22:10:09 -0700

    slider is looking fireeee

--- client/app/components/Swiper.tsx
+++ client/app/components/Swiper.tsx
@@ -24,8 +24,27 @@ const Swiper = () => {
                 alt='slider photo 1'
                 className='w-full h-full object-cover absolute bottom-0 left-0 z-[1]'
               />
-              <div className='z-[10] w-full h-full absolute bottom-0 left-[8.5%] top-[8.5%]'>
-                <h2 className='text-salmonPink'>Trending Item</h2>
+              <div className='z-[10] flex flex-col gap-2 w-full h-full absolute bottom-0 left-[11%] top-[20%]'>
+                <h2 className='text-salmonPink text-[24px] tracking-[1.6px] font-medium max-md:text-lg max-sm:hidden max-xsm:block max-xsm:text-eerieBlack'>
+                  Trending Item
+                </h2>
+                <p className='max-w-[350px] text-4xl font-extrabold text-eerieBlack max-md:text-xl max-md:max-w-[200px] max-xsm:hidden'>
+                  WOMEN'S LATEST FASHION SALE
+                </p>
+                <p className='text-xl text-sonicSilver font-medium max-md:text-base max-xsm:hidden'>
+                  starting at ${' '}
+                  <span className='text-3xl font-bold text-sonicSilver max-md:text-2xl'>
+                    20
+                  </span>
+                  .00
+                </p>
+                <button
+                  className='bg-salmonPink w-fit text-sm
+                  hover:bg-eerieBlack transition-all duration-500 ease
+                py-2 px-5 rounded font-semibold text-white mt-2 max-md:text-[12px] max-md:px-4 max-md:py-1.5 max-xsm:text-[10px] max-xsm:py-1 max-xsm:px-3'
+                >
+                  SHOP NOW
+                </button>
               </div>
             </div>
           </CarouselItem>
