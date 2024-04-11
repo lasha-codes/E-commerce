29dfbaf5be47bcda1affc370e8c116f5c793f5ca
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-10 17:37:36 -0700

    setting up my server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+
+app.use(express.json({ limit: '10mb' }))
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+)
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`)
+})
