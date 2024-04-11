16273df16a590dbd96445b050a385972a1d3d6d0
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-11 00:09:05 -0700

    Product component is done

--- client/app/components/Product.tsx
+++ client/app/components/Product.tsx
@@ -8,22 +8,22 @@ type ProductPropTypes = {
 
 const Product: React.FC<ProductPropTypes> = ({ image, title, count }) => {
   return (
-    <div>
-      <div className='flex items-center'>
-        <div className='bg-spanishGray p-2 rounded-xl border w-[50px] h-[50px]'>
+    <div className='bg-white border max-w-[270px] p-2 flex rounded-xl justify-center items-center'>
+      <div className='flex items-center gap-4 w-full'>
+        <div className='bg-cultured p-2 rounded-xl border w-[65px] h-[50px]'>
           <Image
             src={image}
             priority
-            className='w-full h-full object-cover'
+            className='w-full h-full object-contain'
             alt='product-image'
           />
         </div>
-        <div>
-          <div>
-            <h2>{title}</h2>
-            <span>({count})</span>
+        <div className='flex flex-col items-start gap-1 w-full'>
+          <div className='flex items-center justify-between w-full'>
+            <h2 className='text-[12px] font-semibold'>{title}</h2>
+            <span className='text-[10px] text-sonicSilver'>({count})</span>
           </div>
-          <button>Show All</button>
+          <button className='text-salmonPink text-[12px]'>Show All</button>
         </div>
       </div>
     </div>
