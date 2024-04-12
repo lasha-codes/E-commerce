e8b6d2c0dcc7fa96ca582740da259e3c0b111219
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-11 22:01:00 -0700

    woeking on the basic design of add product

---
+++ client/app/add-product/page.tsx
@@ -0,0 +1,15 @@
+const AddProduct = () => {
+  return (
+    <main className='w-full'>
+      <header className='w-full border-b px-5 py-3'>
+        <div className='flex items-center gap-3'>
+          <h1 className='text-2xl font-medium'>Anon</h1>
+          <div className='h-[22px] w-[1px] bg-spanishGray' />
+          <h3 className='text-sonicSilver'>Add Product</h3>
+        </div>
+      </header>
+    </main>
+  )
+}
+
+export default AddProduct
