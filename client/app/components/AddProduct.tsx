7d037f006e56c25a6f47f4a3c532ccabfce0e09f
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-12 13:07:15 -0700

    starting to work on Review page ui

--- client/app/components/AddProduct.tsx
+++ client/app/components/AddProduct.tsx
@@ -275,148 +275,24 @@ const AddProduct = () => {
             </span>
           </div>
           <div className='flex flex-col gap-10 md:w-[600px] max-md:w-full max-lg:mx-auto'>
-            <h1 className='text-3xl font-medium'>Product Information</h1>
-            <form className='w-full'>
-              <div className='border gap-3 flex items-center rounded-xl p-2'>
-                <div
-                  onClick={() => dispatch(openAddImage())}
-                  className='p-9 w-fit rounded-xl bg-cultured border border-dashed cursor-pointer'
-                >
-                  <FaCirclePlus className='text-3xl' />
-                </div>
+            <div>
+              <div className='flex items-center justify-center gap-10'>
                 {addedImages &&
                   addedImages.map((address, idx) => {
                     return (
-                      <img
-                        key={idx}
-                        src={address}
-                        alt={`added/image${idx}`}
-                        className='h-[100px] rounded-xl w-[120px] object-cover'
-                      />
+                      <div key={idx} className='flex flex-col gap-5'>
+                        <h3 className='text-2xl'>{idx + 1}</h3>
+                        <div className='h-[250px] w-[300px] border rounded-xl overflow-hidden hover:scale-110 transition-all duration-300m ease-in'>
+                          <img
+                            src={address}
+                            className='w-full h-full object-cover hover:zoom-in-50'
+                          />
+                        </div>
+                      </div>
                     )
                   })}
               </div>
-              <div className='flex flex-col gap-4 mt-5 border-b pb-5'>
-                <div className='flex flex-col gap-1'>
-                  <label
-                    htmlFor='name'
-                    className='text-eerieBlack text-[15.5px] max-lg:hidden'
-                  >
-                    Name
-                  </label>
-                  <input
-                    value={name}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                      setName(e.target.value)
-                    }
-                    type='text'
-                    id='name'
-                    className='border rounded px-4 py-0.5 placeholder:text-sm placeholder:opacity-60 placeholder:font-light cursor-pointer'
-                    placeholder='Name'
-                  />
-                </div>
-                <div className='w-full'>
-                  <div className='flex flex-col gap-1'>
-                    <label
-                      htmlFor='desc'
-                      className='text-eerieBlack text-[15.5px] max-lg:hidden'
-                    >
-                      Description
-                    </label>
-                    <textarea
-                      value={description}
-                      onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
-                        setDescription(e.target.value)
-                      }
-                      id='desc'
-                      className='border rounded w-full pt-4 px-4 placeholder:text-sm placeholder:opacity-60 resize-none placeholder:font-light cursor-pointer'
-                      placeholder='Enter product description.'
-                    ></textarea>
-                  </div>
-                </div>
-              </div>
-              <div className='mt-5'>
-                <h2 className='text-xl mb-5 text-oceanGreen'>
-                  Additional info
-                </h2>
-                <div className='flex items-center gap-4'>
-                  <div className='flex flex-col gap-1'>
-                    <input
-                      value={price}
-                      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setPrice(Number(e.target.value))
-                      }
-                      type='number'
-                      id='price'
-                      className='border rounded w-[200px] min-h-[36px] py-0.5 px-4 placeholder:text-sm placeholder:opacity-60 resize-none placeholder:font-light cursor-pointer'
-                      placeholder='0$'
-                    />
-                  </div>
-                  <div>
-                    <Select onValueChange={(value) => setType(value)}>
-                      <SelectTrigger className='w-[200px] rounded'>
-                        <SelectValue placeholder='Product type' />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem className='cursor-pointer' value='sweater'>
-                          Sweater
-                        </SelectItem>
-                        <SelectItem className='cursor-pointer' value='jacket'>
-                          Jacket
-                        </SelectItem>
-                        <SelectItem className='cursor-pointer' value='dress'>
-                          Dress
-                        </SelectItem>
-                        <SelectItem
-                          className='cursor-pointer'
-                          value='Winter Wear'
-                        >
-                          Winter Wear
-                        </SelectItem>
-                        <SelectItem
-                          className='cursor-pointer'
-                          value='Hats & Caps'
-                        >
-                          Hats & Caps
-                        </SelectItem>
-                        <SelectItem
-                          className='cursor-pointer'
-                          value='Shirts & Jeans'
-                        >
-                          Shorts & Jeans
-                        </SelectItem>
-                        <SelectItem className='cursor-pointer' value='Glasses'>
-                          Glasses
-                        </SelectItem>
-                        <SelectItem className='cursor-pointer' value='Watch'>
-                          Watch
-                        </SelectItem>
-                        <SelectItem className='cursor-pointer' value='Perfume'>
-                          Perfume
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                </div>
-              </div>
-              <div className='w-full justify-between flex items-end'>
-                <Link
-                  href='/'
-                  className='underline text-eerieBlack hover:font-medium'
-                  type='button'
-                >
-                  Cancel
-                </Link>
-                <button
-                  onClick={continuePost}
-                  type='button'
-                  className='bg-eerieBlack gap-0.5 flex items-center text-white rounded-full px-5 py-1.5 mt-5 group hover:opacity-80 transition-all duration-300 ease-in-out'
-                >
-                  Continue
-                  <IoArrowForwardOutline className='group-hover:translate-x-2 transition-all duration-300 ease' />
-                </button>
-              </div>
-            </form>
+            </div>
           </div>
         </div>
       </div>
