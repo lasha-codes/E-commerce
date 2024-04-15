bcbab0cc9de9b91e68bb2e37dc3be3c3881072a7
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-15 07:24:50 -0700

    working on the add to cart functionality

--- client/app/components/Products.tsx
+++ client/app/components/Products.tsx
@@ -1,20 +1,21 @@
-import { useSelector } from 'react-redux'
+'use client'
+
+import { useSelector, useDispatch } from 'react-redux'
 import {
   IoHeartOutline,
   IoEyeOutline,
   IoGitCompareOutline,
   IoBagAddOutline,
 } from 'react-icons/io5'
-
-const sideData = [
-  { icon: IoHeartOutline },
-  { icon: IoEyeOutline },
-  { icon: IoGitCompareOutline },
-  { icon: IoBagAddOutline },
-]
+import { addItemToCart } from '../lib/features/tabs/tabsSlice'
 
 const Products = () => {
+  const dispatch = useDispatch()
   const { products } = useSelector((state: any) => state.product)
+
+  const addToCart = (product: any) => {
+    dispatch(addItemToCart(product))
+  }
   return (
     <div className='mt-16'>
       <div className='flex items-center justify-center flex-wrap gap-5'>
@@ -41,18 +42,48 @@ const Products = () => {
                   </div>
                 </div>
                 <div className='absolute right-3 top-2 group-hover:translate-x-0 transition-all duration-300 ease flex flex-col items-center gap-[6px] translate-x-12'>
-                  {sideData.map((item: any) => {
-                    return (
-                      <div className='relative z-[10] side-icon-container overflow-hidden'>
-                        <div className='p-1 rounded-[4px] border transition-all duration-500 side-icon group relative z-[10]'>
-                          <span className='z-[10]'>
-                            <item.icon className='text-[19px] text-sonicSilver cursor-pointer icon-style z-[10]' />
-                          </span>
-                        </div>
-                        <div className='absolute transition-all duration-500 top-0 left-0 rounded-[6px] w-full h-full opacity-0 bg-eerieBlack z-[1]' />
-                      </div>
-                    )
-                  })}
+                  <div className='relative z-[10] side-icon-container overflow-hidden'>
+                    <div className='p-1 rounded-[4px] border transition-all duration-500 side-icon group relative z-[10]'>
+                      <span className='z-[10]'>
+                        <IoHeartOutline className='text-[19px] text-sonicSilver cursor-pointer icon-style z-[10]' />
+                      </span>
+                    </div>
+                    <div className='absolute transition-all duration-500 top-0 left-0 rounded-[6px] w-full h-full opacity-0 bg-eerieBlack z-[1]' />
+                  </div>
+                  <div className='relative z-[10] side-icon-container overflow-hidden'>
+                    <div className='p-1 rounded-[4px] border transition-all duration-500 side-icon group relative z-[10]'>
+                      <span className='z-[10]'>
+                        <IoEyeOutline
+                          className='text-[19px] text-sonicSilver cursor-pointer icon-style z-[10]
+'
+                        />
+                      </span>
+                    </div>
+                    <div className='absolute transition-all duration-500 top-0 left-0 rounded-[6px] w-full h-full opacity-0 bg-eerieBlack z-[1]' />
+                  </div>
+                  <div className='relative z-[10] side-icon-container overflow-hidden'>
+                    <div className='p-1 rounded-[4px] border transition-all duration-500 side-icon group relative z-[10]'>
+                      <span className='z-[10]'>
+                        <IoGitCompareOutline
+                          className='text-[19px] text-sonicSilver cursor-pointer icon-style z-[10]
+'
+                        />
+                      </span>
+                    </div>
+                    <div className='absolute transition-all duration-500 top-0 left-0 rounded-[6px] w-full h-full opacity-0 bg-eerieBlack z-[1]' />
+                  </div>
+                  <div className='relative z-[10] side-icon-container overflow-hidden'>
+                    <div className='p-1 rounded-[4px] border transition-all duration-500 side-icon group relative z-[10]'>
+                      <span className='z-[10]'>
+                        <IoBagAddOutline
+                          onClick={() => addToCart(product)}
+                          className='text-[19px] text-sonicSilver cursor-pointer icon-style z-[10]
+'
+                        />
+                      </span>
+                    </div>
+                    <div className='absolute transition-all duration-500 top-0 left-0 rounded-[6px] w-full h-full opacity-0 bg-eerieBlack z-[1]' />
+                  </div>
                 </div>
               </div>
             )
--- client/app/lib/features/tabs/tabsSlice.ts
+++ client/app/lib/features/tabs/tabsSlice.ts
@@ -1,8 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
+  cartProducts:
+    typeof window !== 'undefined' && localStorage.getItem('cartProducts')
+      ? JSON.parse(localStorage.getItem('cartProducts')!)
+      : [],
   cartOpen: false,
   sideBarOpen: false,
+}
+
+interface ProductType {
+  id: number
+  sold: number
+  price: number
+  image: string[]
+  discountedPrice: null | number
+  title: string
+  description: string
 }
 
 const tabsSlice = createSlice({
@@ -23,10 +37,15 @@ const tabsSlice = createSlice({
     closeSidebar: (state) => {
       state.sideBarOpen = false
     },
+    addItemToCart: (state: any, { payload }: { payload: ProductType }) => {
+      console.log(payload)
+      state.cartProducts.push(payload)
+      localStorage.setItem('cartProducts', JSON.stringify(state.cartProducts))
+    },
   },
 })
 
 export default tabsSlice.reducer
 
-export const { openCart, closeCart, closeSidebar, openSideBar } =
+export const { openCart, closeCart, closeSidebar, openSideBar, addItemToCart } =
   tabsSlice.actions
