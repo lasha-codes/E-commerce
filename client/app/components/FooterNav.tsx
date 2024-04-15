61186a70640ef1a415445fc7e3bfe4a60e958869
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-14 17:35:56 -0700

    footer nav ui is done

--- client/app/components/FooterNav.tsx
+++ client/app/components/FooterNav.tsx
@@ -7,7 +7,7 @@ import { IoIosHeartEmpty } from 'react-icons/io'
 
 const FooterNav = () => {
   return (
-    <div className='fixed bottom-0 left-[27vw] max-lg:left-[21vw] max-md:left-[18vw] w-[590px] rounded-t-xl py-5 drop-shadow-xl bg-[#ffffff]'>
+    <div className='hidden max-lg:block fixed bottom-0 left-[20%] md:w-[590px] w-full max-md:left-0 rounded-t-xl py-5 drop-shadow-xl bg-[#ffffff]'>
       <nav className='flex items-center justify-between px-12'>
         <div className='relative'>
           <IoBagHandleOutline className='text-3xl cursor-pointer icon-style' />
@@ -18,8 +18,11 @@ const FooterNav = () => {
         <div>
           <IoHomeOutline className='text-3xl cursor-pointer icon-style' />
         </div>
-        <div>
+        <div className='relative'>
           <IoIosHeartEmpty className='text-[32.5px] cursor-pointer icon-style' />
+          <div className='absolute bg-salmonPink rounded-full w-[17px] h-[17px] flex items-center justify-center text-white text-sm -top-0.5 -right-1'>
+            0
+          </div>
         </div>
         <div>
           <IoGridOutline className='text-3xl cursor-pointer icon-style' />
