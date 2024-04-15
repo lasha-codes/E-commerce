4c74f73f9f059f7dbaea58dae182070aae8bb68a
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-14 21:47:48 -0700

    add checkIfOpened function to checked if any tabs are opened and based on that display opacity div

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,10 +8,24 @@ import Link from 'next/link'
 import Products from './components/Products'
 import FooterNav from './components/FooterNav'
 import ShoppingCart from './components/ShoppingCart'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+  const { cartOpen } = useSelector((state: any) => state.tabs)
+
+  const checkTabOpened = () => {
+    if (cartOpen) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   return (
     <main className='py-2.5 relative max-lg:pb-20'>
+      {checkTabOpened() && (
+        <div className='fixed z-[10] w-full h-full bg-spanishGray top-0 right-0 opacity-60' />
+      )}
       <Header />
       <Swiper />
       <ProductsSlider />
