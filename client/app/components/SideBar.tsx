eb2286cc9c0a528eda6c5d4f3c8c4f51740adc2d
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-15 02:21:14 -0700

    sidebar design is done and it looks very good

--- client/app/components/SideBar.tsx
+++ client/app/components/SideBar.tsx
@@ -1,12 +1,14 @@
 import Category from './Category'
 import { IoCloseOutline } from 'react-icons/io5'
 import { useSelector, useDispatch } from 'react-redux'
+import { BsCurrencyDollar } from 'react-icons/bs'
 
 const SideBar = () => {
   const dispatch = useDispatch()
   const { products } = useSelector((state: any) => state.product)
 
   interface productType {
+    id: number
     title: string
     price: number
     discountedPrice: null | number
@@ -17,9 +19,11 @@ const SideBar = () => {
   }
 
   return (
-    <div className='fixed flex flex-col items-center px-10 top-0 h-screen z-[99] left-0 w-[500px] bg-white'>
+    <div className='fixed flex flex-col max-md:w-[300px] max-md:pb-20 items-center overflow-hidden max-sm:overflow-y-scroll px-10 top-0 h-screen z-[99] left-0 w-[500px] bg-white'>
       <Category
-        extraClass={'border-none w-[390px] text-xl uppercase'}
+        extraClass={
+          'border-none w-[390px] max-md:w-[200px] -mt-[5px] text-xl uppercase'
+        }
         closeIcon={
           <IoCloseOutline className='text-[25px] absolute top-6 -right-5 icon-style' />
         }
@@ -28,23 +32,23 @@ const SideBar = () => {
         <h2 className='ml-9 text-xl font-semibold text-eerieBlack opacity-95'>
           BEST SELLERS
         </h2>
-        <div className='flex flex-col gap-8 mt-7 ml-5'>
+        <div className='flex items-center flex-wrap gap-7 mt-7 ml-5'>
           {products &&
             products.slice(0, 4).map((product: productType) => {
               return (
-                <div className='flex items-start gap-8'>
+                <div className='flex items-center gap-8' key={product.id}>
                   <div>
                     <img className='w-[65px]' src={product.image[0]} />
                   </div>
                   <div>
                     <h3 className='text-eerieBlack font-medium'>
-                      {product.title.slice(0, 12)}...
+                      {product.title}
                     </h3>
-                    <span className='text-sm text-spanishGray'>
+                    <span className='text-sm text-salmonPink'>
                       {product.type}
                     </span>
-                    <span className='flex items-center'>
-                      <span>$</span>
+                    <span className='flex items-center font-semibold'>
+                      <BsCurrencyDollar />
                       {product.price.toFixed(2)}
                     </span>
                   </div>
