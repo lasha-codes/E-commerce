33efbe8ad702a0cb17ea5381ad9881a689429b4e
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-14 18:54:26 -0700

    close cart is working

--- client/app/components/ShoppingCart.tsx
+++ client/app/components/ShoppingCart.tsx
@@ -1,27 +1,30 @@
 import { HiArrowSmallRight } from 'react-icons/hi2'
 import { FaRegTrashAlt } from 'react-icons/fa'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { closeCart } from '../lib/features/tabs/tabsSlice'
 
 interface tabsTypes {
   cartOpen: boolean
 }
 
 const ShoppingCart = () => {
+  const dispatch = useDispatch()
   const { cartOpen }: tabsTypes = useSelector((state: any) => state.tabs)
-
-  console.log(cartOpen)
 
   return (
     <div
-      className={`fixed h-screen flex flex-col justify-between right-0 top-0 bg-white p-10 transition-all duration-500 ease-out ${
+      className={`fixed h-screen flex flex-col justify-between right-0 top-0 bg-white p-10 transition-all duration-700 ease-in-out ${
         cartOpen
-          ? 'w-[500px] opacity-100 pointer-events-auto'
-          : 'w-0 opacity-0 pointer-events-none'
+          ? 'w-[500px] translate-x-0 pointer-events-auto'
+          : 'w-[500px] translate-x-[550px] pointer-events-none'
       }`}
     >
       <div className='w-full flex justify-between items-center border-b pb-4'>
         <h3 className='font-semibold text-[14px]'>SHOPPING BAG ( 0 )</h3>
-        <HiArrowSmallRight className='text-xl icon-style' />
+        <HiArrowSmallRight
+          className='text-xl icon-style'
+          onClick={() => dispatch(closeCart())}
+        />
       </div>
       <div className='w-full flex flex-col gap-7 border-t pt-6'>
         <div className='flex justify-between items-center'>
