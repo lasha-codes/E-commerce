927366280d60ece927f9af0a960637a920759823
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-14 18:33:57 -0700

    shoppingCart component is done for now

--- client/app/components/ShoppingCart.tsx
+++ client/app/components/ShoppingCart.tsx
@@ -3,21 +3,28 @@ import { FaRegTrashAlt } from 'react-icons/fa'
 
 const ShoppingCart = () => {
   return (
-    <div className='fixed h-screen right-0 top-0 w-[500px] bg-white px-10 py-7'>
+    <div className='fixed h-screen flex flex-col justify-between right-0 top-0 w-[500px] bg-white p-10'>
       <div className='w-full flex justify-between items-center border-b pb-4'>
-        <h3 className='font-semibold text-[14px]'>SHOPPING BAG (0)</h3>
+        <h3 className='font-semibold text-[14px]'>SHOPPING BAG ( 0 )</h3>
         <HiArrowSmallRight className='text-xl icon-style' />
       </div>
-      <div className='w-full'>
+      <div className='w-full flex flex-col gap-7 border-t pt-6'>
         <div className='flex justify-between items-center'>
-          <span>TOTAL: $ 0.00</span>
-          <div className='bg-bitterSweet p-3 rounded-[2.5px] cursor-pointer'>
+          <span className='flex items-center gap-2 font-medium'>
+            <span className='text-[16px]'>TOTAL: </span>
+            <span className='text-[18px]'>$ 0.00</span>
+          </span>
+          <div className='bg-[#f33c3cf5] p-3 rounded-[1px] cursor-pointer hover:opacity-80 transition-all duration-300'>
             <FaRegTrashAlt className='text-white' />
           </div>
         </div>
-        <div>
-          <button>View cart</button>
-          <button>Checkout</button>
+        <div className='flex flex-col gap-2.5'>
+          <button className='bg-cultured font-medium text-[16px] py-3 text-eerieBlack rounded-[2px]'>
+            View cart
+          </button>
+          <button className='text-white bg-eerieBlack text-[16px] py-3 rounded-[2px]'>
+            Checkout
+          </button>
         </div>
       </div>
     </div>
