a75eca9137275f8c812c464471d5cea36b4daff9
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-17 03:58:19 -0700

    working on user auth on backend

---
+++ server/controllers/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import postgres from '../database/postgres.js'
+import bcrypt from 'bcryptjs'
+dotenv.config()
+
+export const registerUser = async (req, res) => {
+  const { email, username, password } = req.body
+  if (!email || !username || !password) {
+    return res.status(400).json({ message: 'All of the fields are required' })
+  }
+  const hashedPassword = await bcrypt.hash(password, 10)
+  console.log(hashedPassword)
+  const query =
+    'INSERT INTO users (email, username, password) VALUES ($1, $2, $3)'
+  const registeredUser = await postgres.query(query, [
+    email,
+    username,
+    hashedPassword,
+  ])
+  console.log(registerUser)
+}
--- server/database/db.sql
+++ server/database/db.sql
@@ -9,3 +9,11 @@ CREATE TABLE products (
 );
 
 ALTER TABLE products ADD COLUMN sold INT NOT NULL DEFAULT 0;
+
+CREATE TABLE users (
+   id SERIAL PRIMARY KEY,
+   picture TEXT,
+   email VARCHAR(100) NOT NULL,
+   username VARCHAR(50) NOT NULL,
+   password VARCHAR(60) NOT NULL
+);
