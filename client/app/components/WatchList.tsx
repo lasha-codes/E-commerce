a89b69e1ade11defad4f72f46aa09249b168536d
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-17 00:10:08 -0700

    working on watchList ui still close to be done for now

--- client/app/components/WatchList.tsx
+++ client/app/components/WatchList.tsx
@@ -1,7 +1,16 @@
+import { HiArrowSmallRight } from 'react-icons/hi2'
+
 const WatchList = () => {
   return (
-    <div className='fixed w-[400px] bg-white drop-shadow-md h-screen'>
-      WatchList
+    <div className='fixed w-[400px] bg-white drop-shadow-md h-screen p-10 right-0 top-0'>
+      <div className='w-full flex items-center justify-between pb-3 border-b'>
+        <h2>WATCH LIST ( 0 )</h2>
+        <HiArrowSmallRight className='icon-style' />
+      </div>
+      <div>{/* watch list products */}</div>
+      <div className='mt-auto pt-5'>
+        <button className=''>Clear Watchlist</button>
+      </div>
     </div>
   )
 }
