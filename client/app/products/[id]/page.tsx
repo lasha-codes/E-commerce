bf25f3071ce294c2d10d26383f07c846c52a4c2a
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-19 05:07:00 -0700

    finding the product based on the id is working

--- client/app/products/[id]/page.tsx
+++ client/app/products/[id]/page.tsx
@@ -1,13 +1,35 @@
 'use client'
 
+import { useSelector } from 'react-redux'
+
 interface ParamsType {
   params: {
     id: any
   }
 }
 
+interface productType {
+  id: number
+  title: string
+  description: string
+  sold: number
+  price: number
+  discountedPrice: number | null
+  image: string[]
+}
+
+interface selectTypes {
+  products: productType[]
+}
+
 const SingleProduct: React.FC<ParamsType> = ({ params }) => {
-  return <div>{params.id.split('%3A')[1]}</div>
+  const { products }: selectTypes = useSelector((state: any) => state.product)
+
+  const productById = products.find((product: productType) => {
+    return product.id === parseInt(params.id)
+  })
+
+  return <div>{productById && productById.title}</div>
 }
 
 export default SingleProduct
