d92bc97d1e027c010e0b92742a04c9ffadb604db
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-19 05:37:05 -0700

    its looking good

--- client/app/products/[id]/page.tsx
+++ client/app/products/[id]/page.tsx
@@ -16,6 +16,7 @@ interface productType {
   price: number
   discountedPrice: number | null
   image: string[]
+  type: string
 }
 
 interface selectTypes {
@@ -29,7 +30,35 @@ const SingleProduct: React.FC<ParamsType> = ({ params }) => {
     return product.id === parseInt(params.id)
   })
 
-  return <div>{productById && productById.title}</div>
+  if (!productById) {
+    return <p>Loading...</p>
+  }
+
+  return (
+    <main className='w-full p-12 bg-white h-screen overflow-y-scroll'>
+      <div className='w-full flex items-start drop-shadow-md bg-white rounded-xl p-8 justify-around gap-10'>
+        <div className='w-[400px] h-[300px] rounded-2xl overflow-hidden'>
+          <img
+            src={productById.image[0]}
+            className='w-full h-full object-contain'
+          />
+        </div>
+        <div className='flex flex-col items-start gap-16'>
+          <div className='flex flex-col gap-3'>
+            <h2 className='text-2xl text-eerieBlack font-medium'>
+              {productById.title}
+            </h2>
+            <span className='text-salmonPink text-lg uppercase font-medium'>
+              {productById.type}
+            </span>
+          </div>
+          <p className='max-w-[500px] text-spanishGray opacity-90 text-[15px]'>
+            {productById.description}
+          </p>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default SingleProduct
