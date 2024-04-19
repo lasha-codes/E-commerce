c1c1c81a5465b235dcbfd093875bb16c16ab861e
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-19 13:15:41 -0700

    star rating logic and ui is looking clean

--- client/app/products/[id]/page.tsx
+++ client/app/products/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import {
   Carousel,
@@ -35,11 +36,15 @@ interface selectTypes {
 }
 
 const SingleProduct: React.FC<ParamsType> = ({ params }) => {
+  const [rating, setRating] = useState<number>(1)
   const dispatch = useDispatch()
   const { products }: selectTypes = useSelector((state: any) => state.product)
   const { watchList }: { watchList: [productType] } = useSelector(
     (state: any) => state.tabs
   )
+  const reviews = [1, 2, 3, 4, 5]
+
+  console.log(rating)
 
   const productById: productType | any = products.find(
     (product: productType) => {
@@ -137,8 +142,30 @@ const SingleProduct: React.FC<ParamsType> = ({ params }) => {
         <div className='w-full flex mt-6 items-center justify-between'>
           <div className='flex flex-col gap-5 items-start'>
             <h2 className='text-eerieBlack text-2xl font-medium'>Reviews</h2>
-            <div className='bg-white w-[400px] h-[235px] pt-4 px-10 drop-shadow-sm rounded-xl'>
+            <div className='bg-white w-[400px] h-[290px] py-6 px-10 drop-shadow-sm rounded-xl'>
               <h3 className='text-xl font-medium mb-3'>Add a review</h3>
+              <div className='flex items-center mb-4'>
+                {reviews.map((star: number) => {
+                  return (
+                    <svg
+                      onClick={() => setRating(star)}
+                      key={star}
+                      xmlns='http://www.w3.org/2000/svg'
+                      fill={star <= rating ? 'black' : 'none'}
+                      viewBox='0 0 24 24'
+                      strokeWidth={1.5}
+                      stroke='currentColor'
+                      className='w-8 h-8 cursor-pointer icon-style transition-all duration-300'
+                    >
+                      <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        d='M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z'
+                      />
+                    </svg>
+                  )
+                })}
+              </div>
               <form className='w-full items-start flex flex-col gap-4'>
                 <div className='flex flex-col items-start gap-3'>
                   <input
