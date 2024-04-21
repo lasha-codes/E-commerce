01f23bbf0d04f40567170fad93a12cbb90e1fe3a
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-21 14:45:29 -0700

    sort products by the rating logic is working

--- client/app/components/Products.tsx
+++ client/app/components/Products.tsx
@@ -13,6 +13,7 @@ interface ProductType {
   title: string
   description: string
   count: number
+  rating: number[]
 }
 
 const Products = () => {
@@ -24,12 +25,25 @@ const Products = () => {
       .sort((a: ProductType, b: ProductType) => b.sold - a.sold)
       .slice(0, 4)
 
+  const compareByReviews = (a: ProductType, b: ProductType) => {
+    const ratingA = a.rating.reduce((acc, number) => acc + number, 0)
+    const ratingB = b.rating.reduce((acc, number) => acc + number, 0)
+    return ratingB - ratingA
+  }
+
+  const sortedByReviews: any =
+    products.length > 0 && [...products].sort(compareByReviews).slice(0, 4)
+
   return (
-    <div className='mt-16 px-28'>
-      <div>
+    <div className='mt-16'>
+      <div className='flex items-center gap-10'>
         <RecommendProducts
           title={'Top Sellers'}
           filteredProducts={mostSoldProducts}
+        />
+        <RecommendProducts
+          title={'By Rating'}
+          filteredProducts={sortedByReviews}
         />
       </div>
       <Toaster />
