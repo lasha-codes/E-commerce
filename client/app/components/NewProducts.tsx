958c69e9cb32b140310736a860bdfb28b8121ffd
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-22 04:03:11 -0700

    visually displaying stars by rating is working

--- client/app/components/NewProducts.tsx
+++ client/app/components/NewProducts.tsx
@@ -26,6 +26,9 @@ const NewProducts = () => {
   }
 
   const { products } = useSelector((state: any) => state.product)
+
+  const starsArray = [1, 2, 3, 4, 5]
+
   return (
     <div className='w-full p-10'>
       <div className='flex items-start justify-center flex-wrap gap-12'>
@@ -34,9 +37,9 @@ const NewProducts = () => {
             return (
               <div
                 key={product.id}
-                className='bg-white border w-[350px] h-[480px] group rounded-[10px] py-10 px-7'
+                className='bg-white border w-[380px] flex justify-center flex-col items-start h-[480px] group rounded-[10px] py-10 px-7'
               >
-                <div className='p-3'>
+                <div className='p-3 w-full flex justify-center'>
                   <img
                     alt='product image'
                     src={product.image[0]}
@@ -50,7 +53,33 @@ const NewProducts = () => {
                   <h2 className='text-[16px] text-sonicSilver font-light'>
                     {product.title}
                   </h2>
-                  <div>{getRating(product.rating)}</div>
+                  <div className='flex items-center'>
+                    {Number(getRating(product.rating))
+                      ? starsArray.map((star: any) => {
+                          return (
+                            <svg
+                              key={star}
+                              xmlns='http://www.w3.org/2000/svg'
+                              fill={
+                                star <= getRating(product.rating)
+                                  ? 'black'
+                                  : 'none'
+                              }
+                              viewBox='0 0 24 24'
+                              strokeWidth={1.5}
+                              stroke='currentColor'
+                              className='w-5 h-5'
+                            >
+                              <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                d='M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z'
+                              />
+                            </svg>
+                          )
+                        })
+                      : 'not rated'}
+                  </div>
                   <span className='flex items-center font-bold'>
                     <LuDollarSign />
                     {product.price}
