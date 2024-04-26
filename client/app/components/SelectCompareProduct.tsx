84031cf075e423a50e968030a0923c6d42fbc1ee
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-26 03:48:00 -0700

    Select compare product component is looking much more better

--- client/app/components/SelectCompareProduct.tsx
+++ client/app/components/SelectCompareProduct.tsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { FiDollarSign } from 'react-icons/fi'
 
 interface ProductType {
   id: number
@@ -13,17 +14,42 @@ interface ProductType {
   rating: number[]
 }
 
-const SelectCompareProduct = () => {
+interface PropTypes {
+  position: string
+  addIndex: number
+}
+
+const SelectCompareProduct = ({ position, addIndex }: PropTypes) => {
   const dispatch = useDispatch()
   const { products }: { products: ProductType[] } = useSelector(
     (state: any) => state.product
   )
+
   return (
     <div className='absolute'>
       <div>
         {products &&
           products.map((product: ProductType) => {
-            return <div key={product.id}></div>
+            return (
+              <div
+                key={product.id}
+                className='flex items-center justify-between w-[250px] rounded-xl'
+              >
+                <img
+                  src={product.image[0]}
+                  alt={`select product image/${product.title}`}
+                  className='h-[70px] w-[100px] object-contain'
+                />
+                <div>
+                  <h3>{product.title}</h3>
+                  <span>{product.type}</span>
+                  <span className='flex items-center'>
+                    <FiDollarSign />
+                    {product.price}
+                  </span>
+                </div>
+              </div>
+            )
           })}
       </div>
     </div>
