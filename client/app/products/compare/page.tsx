1450c15c80f2a96e386e6b6dfd40ae855ddb0f85
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-26 03:35:32 -0700

    workin on the main design of the application

--- client/app/products/compare/page.tsx
+++ client/app/products/compare/page.tsx
@@ -1,6 +1,24 @@
+'use client'
+
 import Link from 'next/link'
+import { useDispatch, useSelector } from 'react-redux'
+import { IoAddOutline } from 'react-icons/io5'
+
+interface ProductType {
+  id: number
+  title: string
+  price: number
+  image: string[]
+  discountedPrice: number | null
+  count: number
+  description: string
+  type: string
+  gender: string
+  rating: number[]
+}
 
 const ProductCompare = () => {
+  const dispatch = useDispatch()
   return (
     <main className='w-full py-5'>
       <header className='px-8'>
@@ -14,7 +32,15 @@ const ProductCompare = () => {
           </span>
         </div>
       </header>
-      <div></div>
+      <section className='w-full px-20 flex justify-center flex-wrap items-center gap-10 mt-28'>
+        <div className='min-w-[500px] cursor-pointer h-[400px] max-md:min-w-full border rounded max-md:h-[350px] max-sm:h-[290px] flex items-center justify-center'>
+          <IoAddOutline className='text-[80px]' />
+        </div>
+
+        <div className='min-w-[500px] cursor-pointer h-[400px] max-md:min-w-full border max-md:h-[350px] max-sm:h-[290px] rounded flex items-center justify-center'>
+          <IoAddOutline className='text-[80px]' />
+        </div>
+      </section>
     </main>
   )
 }
