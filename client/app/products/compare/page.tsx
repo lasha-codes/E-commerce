aac346574ec7b611c64184470e5c81d599b029d6
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-28 21:26:01 -0700

    displaing second products info aswell as getting its avarage rating

--- client/app/products/compare/page.tsx
+++ client/app/products/compare/page.tsx
@@ -45,6 +45,13 @@ const ProductCompare = () => {
         }, 0)
       : 'Not Rated'
 
+  const secondProductRating: any =
+    compareProducts[1].rating?.length > 0
+      ? compareProducts[1].rating.reduce((num, acc) => {
+          return num + acc
+        }, 0)
+      : 'Not Rated'
+
   return (
     <main className='w-full py-5'>
       <header className='px-8'>
@@ -59,7 +66,7 @@ const ProductCompare = () => {
         </div>
       </header>
       <section className='w-full px-20 flex justify-center flex-wrap items-center gap-10 mt-28 mb-5'>
-        <div>
+        <div className='flex flex-col items-start gap-5'>
           <div className='min-w-[500px] cursor-pointer h-[400px] max-md:min-w-full border rounded max-md:h-[350px] relative max-sm:h-[290px] flex items-center justify-center'>
             {!compareProducts[0] ? (
               <IoAddOutline
@@ -90,15 +97,19 @@ const ProductCompare = () => {
             <div className='flex flex-col gap-3'>
               <p className='flex items-start gap-1.5 max-w-[500px]'>
                 <span>Title:</span>
-                <span>{compareProducts[0].title}</span>
+                <span className='text-sonicSilver text-[16px]'>
+                  {compareProducts[0].title}
+                </span>
               </p>
               <p className='flex items-center gap-1.5'>
                 <span>Type:</span>
-                <span>{compareProducts[0].type}</span>
+                <span className='uppercase text-[15px] font-medium'>
+                  {compareProducts[0].type}
+                </span>
               </p>
               <p className='flex items-center gap-1.5'>
                 <span>Price:</span>
-                <span className='flex items-center'>
+                <span className='flex items-center font-semibold'>
                   <FiDollarSign />
                   {compareProducts[0].price}
                 </span>
@@ -120,33 +131,71 @@ const ProductCompare = () => {
             </div>
           )}
         </div>
+        <div className='flex flex-col items-start gap-5'>
+          <div className='min-w-[500px] relative cursor-pointer h-[400px] max-md:min-w-full border max-md:h-[350px] max-sm:h-[290px] rounded flex items-center justify-center'>
+            {!compareProducts[1] ? (
+              <IoAddOutline
+                onClick={() => dispatch(openSecondTab())}
+                className='text-[80px]'
+              />
+            ) : (
+              <div className='relative'>
+                <img
+                  src={compareProducts[1].image[1]}
+                  className='w-[350px] h-[330px] object-contain'
+                />
+                <IoCloseOutline
+                  onClick={() =>
+                    dispatch(removeProductFromComparison({ idx: 1 }))
+                  }
+                  className='absolute top-0 right-0 text-lg hover:text-red-700 transition-all duration-300'
+                />
+              </div>
+            )}
 
-        <div className='min-w-[500px] relative cursor-pointer h-[400px] max-md:min-w-full border max-md:h-[350px] max-sm:h-[290px] rounded flex items-center justify-center'>
-          {!compareProducts[1] ? (
-            <IoAddOutline
-              onClick={() => dispatch(openSecondTab())}
-              className='text-[80px]'
+            <SelectCompareProduct
+              opened={secondCompareOpened}
+              addIndex={1}
+              position='left-12'
             />
-          ) : (
-            <div className='relative'>
-              <img
-                src={compareProducts[1].image[1]}
-                className='w-[350px] h-[330px] object-contain'
-              />
-              <IoCloseOutline
-                onClick={() =>
-                  dispatch(removeProductFromComparison({ idx: 1 }))
-                }
-                className='absolute top-0 right-0 text-lg hover:text-red-700 transition-all duration-300'
-              />
+          </div>
+          {compareProducts[1] && (
+            <div className='flex flex-col gap-3'>
+              <p className='flex items-start gap-1.5 max-w-[500px]'>
+                <span>Title:</span>
+                <span className='text-sonicSilver text-[16px]'>
+                  {compareProducts[1].title}
+                </span>
+              </p>
+              <p className='flex items-center gap-1.5'>
+                <span>Type:</span>
+                <span className='uppercase text-[15px] font-medium'>
+                  {compareProducts[1].type}
+                </span>
+              </p>
+              <p className='flex items-center gap-1.5'>
+                <span>Price:</span>
+                <span className='flex items-center font-semibold'>
+                  <FiDollarSign />
+                  {compareProducts[1].price}
+                </span>
+              </p>
+              <p className='flex items-center gap-1.5'>
+                <span>Sales:</span>
+                <span>{compareProducts[0].sold}</span>
+              </p>
+              <p className='flex items-center gap-1.5'>
+                <span>Rating:</span>
+                <span>
+                  {Number(secondProductRating)
+                    ? (
+                        secondProductRating / compareProducts[1].rating.length
+                      ).toFixed(1)
+                    : 'Not Rated'}
+                </span>
+              </p>
             </div>
           )}
-
-          <SelectCompareProduct
-            opened={secondCompareOpened}
-            addIndex={1}
-            position='left-12'
-          />
         </div>
       </section>
     </main>
