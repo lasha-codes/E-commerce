76c015b804fd5b32af5dbdd672262d8523c5b69a
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-28 20:58:52 -0700

    displaying products info so we can compare them

--- client/app/products/compare/page.tsx
+++ client/app/products/compare/page.tsx
@@ -8,8 +8,8 @@ import {
   openFirstTab,
   openSecondTab,
   removeProductFromComparison,
-  closeTab,
 } from '@/app/lib/features/products/productSlice'
+import { FiDollarSign } from 'react-icons/fi'
 
 interface ProductType {
   id: number
@@ -22,11 +22,12 @@ interface ProductType {
   type: string
   gender: string
   rating: number[]
+  sold: number
 }
 
 const ProductCompare = () => {
   const dispatch = useDispatch()
-  console.log('dasd')
+
   const {
     compareProducts,
     firstCompareOpened,
@@ -50,31 +51,60 @@ const ProductCompare = () => {
         </div>
       </header>
       <section className='w-full px-20 flex justify-center flex-wrap items-center gap-10 mt-28 mb-5'>
-        <div className='min-w-[500px] cursor-pointer h-[400px] max-md:min-w-full border rounded max-md:h-[350px] relative max-sm:h-[290px] flex items-center justify-center'>
-          {!compareProducts[0] ? (
-            <IoAddOutline
-              onClick={() => dispatch(openFirstTab())}
-              className='text-[80px]'
+        <div>
+          <div className='min-w-[500px] cursor-pointer h-[400px] max-md:min-w-full border rounded max-md:h-[350px] relative max-sm:h-[290px] flex items-center justify-center'>
+            {!compareProducts[0] ? (
+              <IoAddOutline
+                onClick={() => dispatch(openFirstTab())}
+                className='text-[80px]'
+              />
+            ) : (
+              <div className='relative'>
+                <img
+                  src={compareProducts[0].image[0]}
+                  className='w-[350px] h-[330px] object-contain'
+                />
+                <IoCloseOutline
+                  onClick={() =>
+                    dispatch(removeProductFromComparison({ idx: 0 }))
+                  }
+                  className='absolute top-0 right-0 text-lg hover:text-red-700 transition-all duration-300'
+                />
+              </div>
+            )}
+            <SelectCompareProduct
+              opened={firstCompareOpened}
+              addIndex={0}
+              position='left-12'
             />
-          ) : (
-            <div className='relative'>
-              <img
-                src={compareProducts[0].image[0]}
-                className='w-[350px] h-[330px] object-contain'
-              />
-              <IoCloseOutline
-                onClick={() =>
-                  dispatch(removeProductFromComparison({ idx: 0 }))
-                }
-                className='absolute top-0 right-0 text-lg hover:text-red-700 transition-all duration-300'
-              />
+          </div>
+          {compareProducts[0] && (
+            <div className='flex flex-col gap-1.5'>
+              <p className='flex items-center gap-1'>
+                <span>Title:</span>
+                {compareProducts[0].title}
+              </p>
+              <p className='flex items-center gap-1'>
+                <span>Type:</span>
+                {compareProducts[0].type}
+              </p>
+              <p className='flex items-center gap-1'>
+                <span>Price:</span>
+                <span className='flex items-center'>
+                  <FiDollarSign />
+                  {compareProducts[0].price}
+                </span>
+              </p>
+              <p className='flex items-center gap-1'>
+                <span>Sales:</span>
+                {compareProducts[0].sold}
+              </p>
+              <p className='flex items-center gap-1'>
+                <span>Description:</span>
+                {compareProducts[0].title}
+              </p>
             </div>
           )}
-          <SelectCompareProduct
-            opened={firstCompareOpened}
-            addIndex={0}
-            position='left-12'
-          />
         </div>
 
         <div className='min-w-[500px] relative cursor-pointer h-[400px] max-md:min-w-full border max-md:h-[350px] max-sm:h-[290px] rounded flex items-center justify-center'>
