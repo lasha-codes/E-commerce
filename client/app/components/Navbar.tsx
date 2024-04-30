d27fe754ff81d59c33a92837dcb28687f3c9968f
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-04-30 04:16:45 -0700

    role based routing function is working

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,41 +1,66 @@
 import Link from 'next/link.js'
-import { navigation } from '../data/data'
+import { navigation, VendorNavigation } from '../data/data'
 import { useSelector } from 'react-redux'
 
 const Navbar = () => {
   const { user } = useSelector((state: any) => state.user)
 
   const returnNavigation = () => {
-    if ((user.role = 'user')) {
+    if (user.role === 'user') {
       return (
-        <div>
+        <nav className='flex items-center gap-7 w-full justify-center py-3 max-md:hidden'>
           {navigation.map((link: any, idx: number) => {
             return (
-              <Link href={link.href} key={idx}>
+              <Link href={link.href} key={idx} className='links'>
                 {link.link}
               </Link>
             )
           })}
-        </div>
+        </nav>
+      )
+    } else if (user.role === 'admin') {
+      return (
+        <nav className='flex items-center gap-7 w-full justify-center py-3 max-md:hidden'>
+          {navigation.map((link: any, idx: number) => {
+            return (
+              <Link href={link.href} key={idx} className='links'>
+                {link.link}
+              </Link>
+            )
+          })}
+        </nav>
+      )
+    } else if (!user.role) {
+      return (
+        <nav
+          className='flex items-center gap-7 w-full 
+        justify-center py-3 max-md:hidden'
+        >
+          {navigation.map((link: any, idx: number) => {
+            return (
+              <Link href={link.href} key={idx} className='links'>
+                {link.link}
+              </Link>
+            )
+          })}
+        </nav>
+      )
+    } else {
+      return (
+        <nav className='flex items-center gap-7 w-full justify-center py-3 max-md:hidden'>
+          {VendorNavigation.map((link: any, idx: number) => {
+            return (
+              <Link href={link.href} key={idx} className='links'>
+                {link.link}
+              </Link>
+            )
+          })}
+        </nav>
       )
     }
   }
 
-  return (
-    <nav className='flex items-center gap-7 w-full justify-center py-3 max-md:hidden'>
-      {
-        (user.role = 'user'
-          ? navigation.map((item, idx) => {
-              return (
-                <Link href={item.href} key={idx} className='links'>
-                  {item.link}
-                </Link>
-              )
-            })
-          : user.role)
-      }
-    </nav>
-  )
+  return <nav>{returnNavigation()}</nav>
 }
 
 export default Navbar
