79481f5b8dfadec820c36c7f0d7ba2541cbcb849
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-08 11:49:23 -0700

    function  in reduix to get all the articles and the builders

--- client/app/lib/features/user/userSlice.ts
+++ client/app/lib/features/user/userSlice.ts
@@ -1,9 +1,20 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:4000'
 
 const initialState = {
   isLoaded: false,
   user: {},
+  articles: [],
 }
+
+export const getArticlesFromDb = createAsyncThunk(
+  'articles/fetchData',
+  async () => {
+    const response = await axios.get('/articles/get-articles')
+    return response.data
+  }
+)
 
 const userSlice = createSlice({
   initialState,
@@ -16,6 +27,17 @@ const userSlice = createSlice({
       state.user = payload
       console.log(state.user)
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getArticlesFromDb.pending, (state: any, action) => {
+      state.articles = []
+    }),
+      builder.addCase(getArticlesFromDb.fulfilled, (state: any, action) => {
+        state.articles = action.payload
+      }),
+      builder.addCase(getArticlesFromDb.rejected, (state: any, payload) => {
+        state.articles = []
+      })
   },
 })
 
