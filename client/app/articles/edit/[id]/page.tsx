05cc0de26aaf780903231541495a725ea4c0c8d4
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-10 03:19:10 -0700

    design is almost done

--- client/app/articles/edit/[id]/page.tsx
+++ client/app/articles/edit/[id]/page.tsx
@@ -1,6 +1,9 @@
 'use client'
+import Link from 'next/link'
 import { useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
+import { FaCirclePlus } from 'react-icons/fa6'
+import { IoIosArrowBack } from 'react-icons/io'
 
 interface articleType {
   image: string
@@ -34,10 +37,64 @@ const EditArticle = ({ params }: { params: { id: string } }) => {
       setNewSummary(articleById.summary)
       setNewSelectedTypes(articleById.types)
     }
-    console.log(newImage, newTitle, newSummary, newSelectedTypes)
   }, [articles])
 
-  return <main className='px-8 py-5 w-full'></main>
+  return (
+    <main className='px-8 py-4 w-full relative'>
+      <header className='w-full pb-4 flex justify-start items-center'>
+        <Link
+          href='/articles'
+          className='icon-style bg-spanishGray opacity-50 p-[6px] rounded-full'
+        >
+          <IoIosArrowBack className='text-xl' />
+        </Link>
+      </header>
+      <div className='w-full flex items-center justify-center mt-10'>
+        <div className='w-[500px] flex flex-col items-start justify-center'>
+          <h2 className='text-eerieBlack text-3xl font-medium'>
+            Write Your Article
+          </h2>
+          <div className='w-full h-[400px] border rounded-xl mt-10 flex justify-center items-center'>
+            <FaCirclePlus className='text-[80px] icon-style' />
+          </div>
+          <div className='w-full mt-10'>
+            <form className='flex flex-col gap-6 items-start'>
+              <div className='w-full flex flex-col items-start gap-1'>
+                <label
+                  htmlFor='title'
+                  className='text-eerieBlack text-lg font-medium'
+                >
+                  New title
+                </label>
+                <input
+                  type='text'
+                  id='title'
+                  className='border rounded px-4 py-1 w-full cursor-pointer placeholder:opacity-80'
+                  placeholder='New title for our article'
+                />
+              </div>
+              <div className='w-full flex flex-col items-start gap-1'>
+                <label
+                  htmlFor='summary'
+                  className='text-eerieBlack text-lg font-medium cursor-pointer'
+                >
+                  New summary
+                </label>
+                <textarea
+                  id='summary'
+                  className='border rounded px-4 py-1 w-full resize-none placeholder:opacity-80'
+                  placeholder='New summary for our article'
+                />
+              </div>
+            </form>
+          </div>
+          <div className='mt-6'>
+            <h3 className='text-lg text-eerieBlack font-medium'>Select Type</h3>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default EditArticle
