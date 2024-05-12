8a5c6d34c7bb29f62b61a75a44ce16d2e1a183e8
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-12 02:21:41 -0700

    design is all done for the articleById

--- client/app/articles/[id]/page.tsx
+++ client/app/articles/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link'
 import { useSelector } from 'react-redux'
+import { format } from 'date-fns'
 
 interface articleType {
   image: string
@@ -34,6 +35,47 @@ const ArticleById = ({ params }: { params: { id: string } }) => {
         <div className='h-[22px] w-[2px] bg-sonicSilver' />
         <h3 className='text-sonicSilver text-sm opacity-80'>Article preview</h3>
       </header>
+      <section className='px-10 mt-10 py-5 w-full flex justify-center items-center'>
+        <div className='flex flex-col items-center gap-6'>
+          <div className='flex flex-col items-start gap-7'>
+            <div className='w-[700px] h-[400px] rounded-xl overflow-hidden'>
+              <img
+                src={articleById?.image}
+                className='h-full w-full object-cover'
+              />
+            </div>
+            <p className='text-sonicSilver text-lg'>
+              {articleById && format(articleById.date, 'MM / dd / yyyy')}
+            </p>
+            <h2 className='text-[20px] text-eerieBlack font-medium'>
+              {articleById?.title}
+            </h2>
+          </div>
+          <div className='flex flex-col w-full items-start gap-3'>
+            <div className='flex justify-center items-center gap-4 w-full flex-wrap px-5'>
+              {articleById?.types.map((type: string, idx: number) => {
+                return (
+                  <div
+                    key={idx}
+                    className='text-lg bg-black text-white px-4 py-1 rounded-full'
+                  >
+                    {type}
+                  </div>
+                )
+              })}
+            </div>
+            <div className='flex flex-col w-full items-start gap-5'>
+              <div className='flex items-center gap-2'>
+                <span>author:</span>
+                <span className='font-semibold'>@{articleById?.author}</span>
+              </div>
+              <p className='w-[580px] text-sonicSilver'>
+                {articleById?.summary}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
